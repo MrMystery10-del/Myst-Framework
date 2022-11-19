fb927384fd3d593403e5edc42ef34b4e4fd6a10d
Mr Mystery 1.0 <69641472+MrMystery10-del@users.noreply.github.com>
2022-11-19 15:31:32 +0100

    Updated Core.java
    Merged the main method with the core, changed the code to look bit more clean

--- Myst/Base Structure/source/base/Core.java
+++ Myst/Base Structure/source/base/Core.java
@@ -4,30 +4,36 @@ import gui.Frame;
 import gui.Panel;
 
 /**
- * The Main core of the program
+ * The starting point of the programm
  * 
- * @version 1.0
+ * @version 1.1
  * @author MrMystery10-del
  */
 public class Core {
 
-    Panel panel;
+    private Panel panel;
 
-    /** This is the core of the game where the logic is */
-    public Core() {
+    private short panelWidth = 1200;
+    private short panelHeight = 800;
+
+    // Programm start
+    public static void main(String[] args) {
+        new Core();
+    }
+
+    // Non-static instance for running non-static methods
+    public Core(){
         createGUI();
         repaintLoop();
     }
 
     // This method creates the GUI
     private void createGUI() {
-        short width = 1200;
-        short height = 800;
-        panel = new Panel(width, height);
-        new Frame(width, height, new Panel(width, height));
+        panel = new Panel(panelWidth, panelHeight);
+        new Frame(panelWidth, panelHeight, panel);
     }
 
-    // This method is a infinity loop to repaint the screen with around 60 FPS
+    // This method is a infinity loop to repaint the screen with around 60 FPS (Only works when something on the GUI happens)
     private void repaintLoop() {
         new Thread(new Runnable() {
             public void run() {
